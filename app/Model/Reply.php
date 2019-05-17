b502261873ed2e13623744fa67eab50abee5000a
graziantonio <antonius1088@gmail.com>
2019-05-17 20:45:14 +0200

    Relationship

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -3,8 +3,17 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\Console\Question\Question;
 
 class Reply extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+    public function like(){
+        return $this->hasMany(Like::class);
+    }
 }
